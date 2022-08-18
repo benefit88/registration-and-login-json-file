3daecaae2d4d9eb265f2c0ddd39c8eadae7b8e36
benefit88 <65952017+benefit88@users.noreply.github.com>
2022-08-18 15:57:59 +0300

    Update profile.php

--- profile.php
+++ profile.php
@@ -38,7 +38,9 @@ require 'session.php';
 </nav>
 <div class="jumbotron jumbotron-fluid">
     <div class="container">
-        <h1 class="display-4"> Hello <?php echo $_SESSION['username']?>   </h1>
+        <h1 class="display-4"> Hello <?= $name; ?>    </h1>
+        <h1 class="display-4"> Ваш логин: <?php echo $_SESSION['username']?>    </h1>
+        <h1 class="display-4"> Ваш email: <?= $email; ?>    </h1>
         <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
     </div>
 </div>
