08be5f882e826de9fcf1d43ecfc4c40e900d56c3
benefit88 <65952017+benefit88@users.noreply.github.com>
2022-08-18 15:57:03 +0300

    Update session.php

--- session.php
+++ session.php
@@ -3,4 +3,13 @@ session_start();
 $user=$_SESSION['username'];
 $json = file_get_contents('data.json');
 $json_data = json_decode($json,true);
+$uname=$_SESSION['username'];
+foreach ($json_data as $key=>$value)
+{
+    if($value['uname']==$uname){
+        $name=$value['name'];
+        $email=$value['email'];
+
+    }
+}
 $username=$user;
