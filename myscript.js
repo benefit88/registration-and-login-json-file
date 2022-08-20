71a5f23ea4844813e9b4b5c997bd91d0f66b8fb8
benefit88 <65952017+benefit88@users.noreply.github.com>
2022-08-20 13:39:40 +0300

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,12 +1,27 @@
-jQuery.validator.addMethod("lettersonly", function(value, element)
+jQuery.validator.addMethod("lettersOnly", function(value, element)
 {
     return this.optional(element) || /^[a-zA-Zа-яёА-ЯЁ]+$/i.test(value);
 });
 
-jQuery.validator.addMethod("checkpass", function(value, element)
+jQuery.validator.addMethod("checkPass", function(value, element)
 {
     return this.optional(element) || /^(?=.*[0-9])(?=.*[a-zA-Zа-яёА-ЯЁ]).{6,30}$/g.test(value) ;
 });
+
+jQuery.validator.addMethod("checkSpace",function (value, element) {
+    return this.optional(element) || /^[^\s]*$/.test(value);
+});
+
+jQuery.validator.addMethod("checkEmail",function (value, element) {
+    return this.optional(element) || /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value);
+});
+
+
+
+
+
+
+
 
     $(document).ready(function (){
     $("#register-btn").click(function (){
@@ -19,48 +34,63 @@ jQuery.validator.addMethod("checkpass", function(value, element)
 });
     $("#login-frm").validate({
         rules: {
-            username:{
+            username:{<!--        логин-->
                 required:true,
+                checkSpace:true,
                 minlength:6
+
             },
             password:{
+                checkSpace:true,
                 required:true,
                 minlength:6,
-                checkpass:true
+                checkPass:true
             }
 
         },
         messages:{
             username: {
                 required: "Заполните это поле",
-                minlength: "Имя должно быть не менее 6 символов",
+                minlength: "Логин должен быть не менее 6 символов",
+                checkSpace:"Логин не должен содержать пробелов"
             },
             password:{
                 required:"Заполните это поле",
                 minlength:"Пароль должен быть не менее 6 символов",
-                checkpass: "Пароль должен содержать буквы и цифры"
+                checkPass:"Пароль должен содержать буквы и цифры",
+                checkSpace:"Пароль не должен содержать пробелов"
+
             }
         }
 
     });
     $("#register-frm").validate({
     rules:{
-        name:{
+        name:{ <!--        имя-->
             required:true,
             minlength:2,
-            lettersonly: true
+            lettersOnly: true,
+            checkSpace:true
+
         },
-        uname:{
+        uname:{<!--        логин-->
             required:true,
-            minlength:6
+            minlength:6,
+            checkSpace:true,
+
         },
         email:{
-            required:true
+            required:true,
+            checkSpace:true,
+            checkEmail:true
+
         },
         pass:{
             required:true,
             minlength:6,
-            checkpass:true
+            checkPass:true,
+            checkSpace:true
+
         },
 
         cpass:{
@@ -69,22 +99,32 @@ jQuery.validator.addMethod("checkpass", function(value, element)
         }
     },
     messages:{
-        name:{
+        name:{<!--        имя-->
             required: "Заполните это поле",
             minlength: "Имя должно быть не менее 2 символов",
-            lettersonly: "Только буквы"
+            lettersOnly: "Только буквы",
+            checkSpace:"Имя не должно содержать пробелов"
+
         },
-        uname:{
+        uname:{<!--        логин-->
             required: "Заполните это поле",
-            minlength: "Имя должно быть не менее 6 символов",
+            minlength: "Логин должен быть не менее 6 символов",
+            checkSpace:"Логин не должен содержать пробелов"
+
         },
         email:{
-            required:"Проверьте правильность email"
+
+            required:"Проверьте правильность email",
+            checkSpace:"Email не должен содержать пробелов",
+            checkEmail:"Проверьте правильность email"
+
         },
         pass:{
             required:"Заполните это поле",
             minlength:"Пароль должен быть не менее 6 символов",
-            checkpass: "Пароль должен содержать буквы и цифры"
+            checkPass: "Пароль должен содержать буквы и цифры",
+            checkSpace:"Пароль не должен содержать пробелов"
+
         },
         cpass: {
             equalTo: "Пароли не совпадают",
